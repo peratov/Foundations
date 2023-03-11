978ff8daa30e2a008af1d948f56fc041ef06a56e
Kenneth Anku <kennth.anku@code.berlin>
2023-03-11 18:07:01 +0100

    Setup Complete

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
+@app.route('/features')
+def features():
+    return '<h1>Learn about our features</h1>!'
+
+@app.route('/about')
+def about():
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run()
